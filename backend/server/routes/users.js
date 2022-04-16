b7e83742720169caded00fc8acfc9259ad0a4f89
Nishant Balaji <nishantb1130@gmail.com>
2022-04-16 11:17:03 -0700

    update backend andstart map component

--- backend/server/routes/users.js
+++ backend/server/routes/users.js
@@ -16,4 +16,16 @@ router.post('/createUser', async (req, res) => {
     }
 });
 
+router.put('/updateUser', async (req, res) => {
+    const {password, email} = req.body
+    if (!password || !email) {
+        res.status(400).json({ error: 'Invalid Input' });
+    } else {
+        const updatedUser = await User.findOneAndUpdate({email: email}, {password: password}, {new: true});
+        res.status(200).json(updatedUser);
+    }
+});
+
+
+
 module.exports = router;
