40ed590436ab09c60eac9ba0eb6cea8a16be0c55
William Hu <williamhu211@gmail.com>
2022-04-16 13:52:27 -0700

    getUsers route

--- backend/server/routes/users.js
+++ backend/server/routes/users.js
@@ -15,20 +15,9 @@ router.post('/createUser', async (req, res) => {
     res.status(200).json(newUser);
 });
 
-
-
-/*
-router.put('/updateUser', async (req, res) => {
-    const {password, email} = req.body
-    if (!password || !email) {
-        res.status(400).json({ error: 'Invalid Input' });
-    } else {
-        const updatedUser = await User.findOneAndUpdate({email: email}, {password: password}, {new: true});
-        res.status(200).json(updatedUser);
-    }
+router.post('/getUsers', async (req, res) => {
+    const users = await User.find();
+    res.status(200).json(users);
 });
-*/
-
-
 
 module.exports = router;
