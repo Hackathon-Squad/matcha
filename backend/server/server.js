b7a1432ae44819354616b3e622b1efbfeb2a6fe4
Nishant Balaji <nishantb1130@gmail.com>
2022-04-16 09:29:02 -0700

    initialize backend

---
+++ backend/server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const mongodb = require('mongodb');
+const cors = require('cors');
+const users = require('./routes/users');
+
+const app = express();
+const url = ``;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', users);
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true }).then(() => {
+  console.log('Connected to MongoDB database');
+});
+
+app.listen(4000, () => {
+    console.log(`Starting server on port 4000`);
+  });
