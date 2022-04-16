da995abaf2143ced5a100890d10f21476a1026f8
sq19 <shqi@ucsd.edu>
2022-04-16 11:09:45 -0700

    gooogle

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,8 +1,21 @@
 import './style.scss'
-const Home = () => {
+import GoogleLogin from 'react-google-login'
+
+const Home = ({ handleSuccessfulLogin, handleFailedLogin}) => {
   return <div className="home">
     <h1 className="home-title">CoffeeChat</h1>
-    <button className="home-signin">sign up with google</button>
+    <GoogleLogin
+        className="google-login"
+        clientId={'956626273843-t281em1k26amkus44arum6lk434gr8su.apps.googleusercontent.com'}
+        buttonText="Login"
+        onSuccess={handleSuccessfulLogin}
+        onFailure={handleFailedLogin}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={false}
+        render={(renderProps) => (
+          <button className="home-signin" onClick={renderProps.onClick}>sign up with google</button>
+        )}
+      />
   </div>
 }
 
