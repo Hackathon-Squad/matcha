1bfe92a34d5c0604fe4f6e5e2031f1ec0caafefe
Faris Ashai <farisashai@gmail.com>
2022-04-16 22:13:29 -0700

    user matching callback function

--- frontend/src/pages/find.tsx
+++ frontend/src/pages/find.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
 import Navbar from "@/components/Navbar";
@@ -16,17 +16,25 @@ const FindUsers: NextPage<FindUsersProps> = ({}) => {
   const [nearbyUsers, setNearbyUsers] =
     useState<GoogleUserResponse[]>(userData);
 
+  const [currentUser, setCurrentUser] = useState<GoogleUserResponse>(nearbyUsers[0]);
+  
+  const handleAccept = useCallback(() => {
+    console.log(`${user?.name} matches with ${currentUser.name}!`)
+    // TODO: Make a post requrest to store currentUser in users matches
+  }, [currentUser, user])
+
+  const handleReject = useCallback(() => {
+    console.log(`${user?.name} rejects ${currentUser.name}!`)
+    // Move on
+  }, [currentUser, user])
+  
   return (
     <>
       <Navbar />
       <SwipeOnUser
-        user={nearbyUsers[0]}
-        onReject={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-        onAccept={function (): void {
-          throw new Error("Function not implemented.");
-        }}
+        user={currentUser}
+        onReject={handleReject}
+        onAccept={handleAccept}
       />
     </>
   );
