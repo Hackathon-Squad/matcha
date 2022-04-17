0ded5909b217f1f9f89ae4f72dbe30f243171b82
Faris Ashai <farisashai@gmail.com>
2022-04-17 05:09:49 -0700

    boba balls

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,8 +4,15 @@ import { useRouter } from "next/router";
 import GoogleLogin from "react-google-login";
 import { useUserContext } from "@/context/UserContext";
 import styles from "./index.module.scss";
-
 import logo from "assets/logo-final.svg";
+
+const boba1 = 'assets/boba1.svg'
+const boba2 = 'assets/boba2.svg'
+const boba3 = 'assets/boba3.svg'
+const boba4 = 'assets/boba4.svg'
+const boba5 = 'assets/boba5.svg'
+const boba6 = 'assets/boba6.svg'
+
 
 const Home: NextPage = () => {
   const router = useRouter();
@@ -17,9 +24,27 @@ const Home: NextPage = () => {
       router.replace("/find");
     }
   }, [router, user]);
-
+  
   return (
     <div className={styles["home"]}>
+      {/* <img src={boba1} id="boba1" alt="" className={styles.boba} />
+      <img src={boba2} id="boba2" alt="" className={styles.boba} />
+      <img src={boba3} id="boba3" alt="" className={styles.boba} />
+      <img src={boba4} id="boba4" alt="" className={styles.boba} />
+      <img src={boba5} id="boba5" alt="" className={styles.boba} />
+      <img src={boba6} id="boba6" alt="" className={styles.boba} /> */}
+      <img src={boba6} id="boba7" alt="" className={styles.boba} />
+      <img src={boba2} id="boba8" alt="" className={styles.boba} />
+      <img src={boba3} id="boba9" alt="" className={styles.boba} />
+      <img src={boba4} id="boba10" alt="" className={styles.boba} />
+      <img src={boba5} id="boba11" alt="" className={styles.boba} />
+      <img src={boba2} id="boba12" alt="" className={styles.boba} />
+      <img src={boba6} id="boba13" alt="" className={styles.boba} />
+      <img src={boba1} id="boba14" alt="" className={styles.boba} />
+      <img src={boba4} id="boba15" alt="" className={styles.boba} />
+      <img src={boba3} id="boba16" alt="" className={styles.boba} />
+      <img src={boba6} id="boba17" alt="" className={styles.boba} />
+  
       <img className={styles["logo"]} src={logo.src} alt="" />
       <h1 className={styles["home-title"]}>Matcha</h1>
       <GoogleLogin
